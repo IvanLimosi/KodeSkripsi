cd8b5e8c380fd8963004c0f5f2e1d66480b6608a
Ivan Limosi <72721514+IvanLimosi@users.noreply.github.com>
2022-12-10 21:51:58 +0700

    add frame bank ma1ware

--- main.py
+++ main.py
@@ -65,9 +65,47 @@ def openBank():
     top.title("Bank Malware")
 
     frameA = LabelFrame(top, text="Cerber", padx=10,pady=10)
-    frameA.grid(row=0,column=0,padx=10)
-    btn_grayscaleA = Button(frameA, text="Grayscale")
+    frameA.grid(row=0,column=0,padx=10, pady=10)
+    btn_grayscaleA = Button(frameA, text="Grayscale", padx= 13)
     btn_grayscaleA.pack()
+    btn_entropyA = Button(frameA, text="Entropy Graph" )
+    btn_entropyA.pack()
+
+    frameB = LabelFrame(top, text="Cerber", padx=10,pady=10)
+    frameB.grid(row=0,column=1,padx=10, pady=10)
+    btn_grayscaleB = Button(frameB, text="Grayscale", padx= 13)
+    btn_grayscaleB.pack()
+    btn_entropyB = Button(frameB, text="Entropy Graph" )
+    btn_entropyB.pack()
+
+    frameC = LabelFrame(top, text="Cerber", padx=10,pady=10)
+    frameC.grid(row=0,column=2,padx=10, pady=10)
+    btn_grayscaleC = Button(frameC, text="Grayscale", padx= 13)
+    btn_grayscaleC.pack()
+    btn_entropyC = Button(frameC, text="Entropy Graph" )
+    btn_entropyC.pack()
+
+    frameD = LabelFrame(top, text="Cerber", padx=10,pady=10)
+    frameD.grid(row=1,column=0,padx=10, pady=10)
+    btn_grayscaleD = Button(frameD, text="Grayscale", padx= 13)
+    btn_grayscaleD.pack()
+    btn_entropyD = Button(frameD, text="Entropy Graph" )
+    btn_entropyD.pack()
+
+    frameE = LabelFrame(top, text="Cerber", padx=10,pady=10)
+    frameE.grid(row=1,column=1,padx=10, pady=10)
+    btn_grayscaleE = Button(frameE, text="Grayscale", padx= 13)
+    btn_grayscaleE.pack()
+    btn_entropyE = Button(frameE, text="Entropy Graph" )
+    btn_entropyE.pack()
+
+    frameF = LabelFrame(top, text="Cerber", padx=10,pady=10)
+    frameF.grid(row=1,column=2,padx=10, pady=10)
+    btn_grayscaleF = Button(frameF, text="Grayscale", padx= 13)
+    btn_grayscaleF.pack()
+    btn_entropyF = Button(frameF, text="Entropy Graph" )
+    btn_entropyF.pack()
+
 
 #button untuk upload file
 btn_upload = Button(frame, text="Upload File",command=upload)
